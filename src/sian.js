79de038e77bc3a615064581d53657d850dbf860c
RhysAnexture <rhys@anexture.com>
2018-04-07 14:56:55 -0400

    refactored in order to use the beaker browser in the simplest way possible. No more npm i

--- src/sian.js
+++ src/sian.js
@@ -9,6 +9,32 @@ const connect = new Connect('SIAN', {
 
 const web3 = connect.getWeb3()
 
+const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
+
+const StatusContract = web3.eth.contract(abi);
+const statusInstance = StatusContract.at('0x70A804cCE17149deB6030039798701a38667ca3B')
+
+// uPort connect
+const uportConnect = function () {
+  web3.eth.getCoinbase((error, address) => {
+    if (error) { throw error }
+    globalState.ethAddress = address
+
+    // This one is for display purposes - MNID encoding includes network 
+    globalState.uportId = window.uportconnect.MNID.encode({network: '0x4', address: address})
+
+    statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
+      globalState.currentStatus = st
+      web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
+        globalState.ethBalance = web3.fromWei(bal)
+        render()
+      })
+    })
+  })
+}
+
+
+/*
 // Request credentials to login
 connect.requestCredentials({
   requested: ['name', 'phone', 'country'],
@@ -27,3 +53,5 @@ connect.attestCredentials({
   },
   exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
 })
+
+*/
